1dd9e9b98cb006dcacb6251d7ff0341587f26aee
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-09-30 19:05:44 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,6 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+nodejs_version=16
+YNH_COMPOSER_VERSION="2.2.7"
